3721a4183af48e86ba15ef3a57cefd955609373b
Ishita Saha <129981334+IshitaSaha2608@users.noreply.github.com>
2025-02-08 00:21:01 +0530

    Update ContactUs.jsx

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -8,48 +8,47 @@ export default function ContactUs() {
 
 
     return(
-        <section className="py-16 px-32 bg-[#0F1318]">
+        <section className="py-16 px-32 bg-[#41362A]">
         <h2 className="text-2xl font-bold text-white text-center mb-12">CONTACT US</h2>
         <div className="max-w-2xl mx-auto">
           <form className="space-y-6">
             <div>
-              <label htmlFor="name" className="block text-sm font-medium text-gray-400">
+              <label htmlFor="name" className="block text-sm font-medium text-white">
                 Name
               </label>
               <input
                 type="text"
                 id="name"
                 name="name"
-                className="mt-1 block w-full rounded-md bg-[#1A1E24] border-gray-700 shadow-sm focus:border-[#4DB6AC] focus:ring focus:ring-[#4DB6AC] focus:ring-opacity-50"
+                className="mt-1 block w-full rounded-md bg-[#EDE5D9] border-gray-700 bg-opacity-50 shadow-sm focus:border-[#1A1E24] focus:ring focus:ring-[#1A1E24] focus:ring-opacity-50"
               />
             </div>
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-400">
+              <label htmlFor="email" className="block text-sm font-medium text-white">
                 Email
               </label>
               <input
                 type="email"
                 id="email"
                 name="email"
-                className="mt-1 block w-full rounded-md bg-[#1A1E24] border-gray-700 shadow-sm focus:border-[#4DB6AC] focus:ring focus:ring-[#4DB6AC] focus:ring-opacity-50"
+                className="mt-1 block w-full rounded-md bg-[#EDE5D9] border-gray-700 bg-opacity-50 shadow-sm focus:border-[#4DB6AC] focus:ring focus:ring-[#4DB6AC] focus:ring-opacity-50"
               />
             </div>
             <div>
-              <label htmlFor="message" className="block text-sm font-medium text-gray-400">
+              <label htmlFor="message" className="block text-sm font-medium text-white">
                 Message
               </label>
               <textarea
                 id="message"
                 name="message"
                 rows={4}
-                className="mt-1 block w-full rounded-md bg-[#1A1E24] border-gray-700 shadow-sm focus:border-[#4DB6AC] focus:ring focus:ring-[#4DB6AC] focus:ring-opacity-50"
+                className="mt-1 block w-full rounded-md bg-[#EDE5D9] border-gray-700 bg-opacity-50 shadow-sm focus:border-[#4DB6AC] focus:ring focus:ring-[#4DB6AC] focus:ring-opacity-50"
               ></textarea>
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#D77E1D] hover:bg-[#3D9089] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#4DB6AC]"
-              >
+                className="w-full flex justify-center py-2 px-4 border border-[#D77E1D] rounded-md  bg-transparent border-2 border-[#D77E1D] text-white font-bold py-4 px-16 hover:bg-[#D77E1D] hover:text-white transition-all uppercase tracking-wider">
                 Send Message
               </button>
             </div>
